a2ad9df48944244279778e1ed206ac55ac2924b0
Greg Bowyer <gbowyer@fastmail.co.uk>
2010-02-12 14:05:12 +0000

    Pushed up a bunch of stupid clojure blobs
        - Improved the clojure <-> coherence script a little
        - JMX code that allows clojure to bind a local JVM for JMX
            via the Attach API
        - Maven project that can be used as a stupid but convenient shortcut to
            starting up clojure nailgun and swank shells, replete with required
            classpath noise

--- clojure-cache-console.clj
+++ clojure-cache-console.clj
@@ -1,29 +1,50 @@
 (ns ctr 
  (:require (clojure stacktrace inspector) (clojure.contrib repl-utils))
- (:require (clojure stacktrace inspector) (clojure.contrib repl-utils pprint))
- (:import com.oracle.coherence.common.identifiers.Identifier)
- (:import com.shopzilla.site.service.clickrate.domain.ClickThroughRate))
+ (:require clojure.contrib.pprint)
+ (:require clojure.contrib.duck-streams))
 
-(defn setup-env[name] 
+(defn setup-env [name port]
   (doseq [tuple
     [ '("com.sun.management.jmxremote" "true")
       '("tangosol.coherence.management" "all")
       '("tangosol.coherence.log.level" "2")
-      '("tangosol.pof.config" "ctr-pof-config.xml")
-      '("tangosol.pof.enabled" "true")
       '("tangosol.coherence.distributed.localstorage" "false")
-
-      `("tangosol.coherence.override" ~(format "tangosol-coherence-override-local-%s.xml" name))
-      `("tangosol.coherence.cacheconfig" ~(format "ctr-%s-cache-config.xml" name))
-      `("visualvm.display.name" ~(format "'%s Coherence Clojure Console'" name))]]
+      '("tangosol.pof.enabled" "true")
+      '("tangosol.pof.config" "ctr-pof-config.xml")
+      `("tangosol.coherence.clusterport" ~(str port))
+      `("tangosol.coherence.override" ~(format "tangosol-coherence-override-%s-dev.xml" name))
+      `("tangosol.coherence.cacheconfig" ~(format "clickrate-%s-cache-config.xml" name))
+      `("visualvm.display.name" ~(format "'%s-Clojure-Console'" name))]]
 
     (let [[name value] tuple]
       (prn (format "Set %s --> %s" name value))
       (. System (setProperty name value))
       (assert (= value (. System (getProperty name)))))))
- 
-;(def *grid* (. com.tangosol.net.CacheFactory (getCache "Impressions")))
 
+(defn bind-env [env-name port]
+  "Shortcut function to bind to the given cluster"
+  (do
+    (ns ctr)
+    (setup-env env-name port)
+    (. com.tangosol.net.CacheFactory ensureCluster)))
+ 
+(defn get-grid [gridname]
+  (. com.tangosol.net.CacheFactory (getCache gridname)))
+
+(defn dump-grid [grid file]
+  (clojure.contrib.duck-streams/write-lines file
+    (map #(let [[k v] %] (format "%s=%s" k (bean v))) grid)))
+
+(defn print-grid [gridname]
+  "Prints out an entire grid to stdout"
+  (map #(let [[k v] %]
+          (print "\n")
+          (clojure.contrib.pprint/pprint k)
+          (clojure.contrib.pprint/pprint (bean v))
+          (print "--------------------------------------------------\n"))
+       (get-grid gridname)))
+
+(comment
 (defn create-basic-processor [] 
   "Create a basic processor to be attached to a grid for processing"
   (proxy [com.tangosol.util.processor.AbstractProcessor com.tangosol.io.pof.PortableObject] []
@@ -35,5 +56,4 @@
     (readExternal [reader] )
     (writeExternal [writer] )))
 
- (. *grid* (invoke 10 (create-basic-processor)))
-
+ (. *grid* (invoke 10 (create-basic-processor))))
